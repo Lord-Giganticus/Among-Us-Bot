48690f8b123aa46d9f1323bd5087fa7818850fcc
Mattamech 3.0 <mattamech3.0@gmail.com>
2020-10-09 08:27:13 -0600

    Adding new commands to both bots

--- nodejs/among-us-bot.js
+++ nodejs/among-us-bot.js
@@ -12,11 +12,15 @@ client.once('ready', async () => {
 client.on('message', message => {
 
     if(message.content.startsWith(`${prefix}info`)) {
-        message.channel.send("Among Us Bot commands: ```A!info: Displays this. A!invite: Sends the bot invite A!join: Joins the VC you are in. A!dis: Disconects from the VC. A!mute: Mutes all members in the VC. A!unmute: Unmutes all members in the VC.```")
+        message.channel.send("Among Us Bot commands: ```A!info: Displays this. A!invite: Sends the bot invite A!backup: Sends the backup bot's invite. A!join: Joins the VC you are in. A!dis: Disconects from the VC. A!mute: Mutes all members in the VC. A!unmute: Unmutes all members in the VC.```")
     }
 
     if(message.content.startsWith(`${prefix}invite`)) {
         message.channel.send("https://discord.com/oauth2/authorize?client_id=763810277548228680&scope=bot&permissions=22039552")
+    }
+
+    if(message.content.startswith(`${prefix}backup`)) {
+        message.channel.send("https://discord.com/oauth2/authorize?client_id=764110739459670036&scope=bot&permissions=22039552")
     }
 
     if(message.content.startsWith(`${prefix}join`)) {
--- nodejs/backup-bot/backup-among-us-bot.js
+++ nodejs/backup-bot/backup-among-us-bot.js
@@ -12,11 +12,15 @@ client.once('ready', async () => {
 client.on('message', message => {
 
     if(message.content.startsWith(`${prefix}info`)) {
-        message.channel.send("Among Us Bot commands: ```A!info: Displays this. A!invite: Sends the bot invite A!join: Joins the VC you are in. A!dis: Disconects from the VC. A!mute: Mutes all members in the VC. A!unmute: Unmutes all members in the VC.```")
+        message.channel.send("Backup Among Us Bot commands: ```A2!info: Displays this. A2!invite: Sends the normal bot invite A2!backup: Sends this bot's invite. A2!join: Joins the VC you are in. A2!dis: Disconects from the VC. A2!mute: Mutes all members in the VC. A2!unmute: Unmutes all members in the VC.```")
     }
 
     if(message.content.startsWith(`${prefix}invite`)) {
         message.channel.send("https://discord.com/oauth2/authorize?client_id=763810277548228680&scope=bot&permissions=22039552")
+    }
+
+    if(message.content.startsWith(`${prefix}backup`)) {
+        message.channel.send("https://discord.com/oauth2/authorize?client_id=764110739459670036&scope=bot&permissions=22039552")
     }
 
     if(message.content.startsWith(`${prefix}join`)) {
