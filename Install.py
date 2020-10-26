4f0afe49432914b2528ad57db7d1838978df8730
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2020-10-26 07:23:56 -0600

    Added a installer python script for windows
    This allows for a much easier installing of the `node_modules`.

---
+++ Install.py
@@ -0,0 +1,50 @@
+import os
+
+Download_Location = input("Enter where you want the repo to be cloned:")
+os.chdir(Download_Location)
+print(os.getcwd())
+input("If this is not correct this WILL save somewhere you didn't want it. Press enter to continue")
+
+git_location = input("Enter either your forked repo's .git link or the original repo's .git link:")
+
+file = open('DL.bat','w')
+file.write('git clone ')
+file.write(git_location)
+file.close()
+
+os.system('cmd /c DL.bat')
+os.system('cmd /c del /f DL.bat')
+
+Bot_Choice = input("Enter a number for which bot you want to work on.\n[1] Normal Bot\n[2] Backup Bot\n[3] Both\n")
+if Bot_Choice == "1":
+    Repo_Folder = input("Enter the location of the cloned folder:")
+    os.chdir(Repo_Folder)
+    print(os.getcwd())
+    input("If this is not correct this WILL NOT work. Press enter to continue.")
+    os.chdir("nodejs")
+    os.system('cmd /c npm install')
+    input("Complete. Press enter to exit.")
+    exit()
+elif Bot_Choice == "2":
+    Repo_Folder = input("Enter the location of the cloned folder:")
+    os.chdir(Repo_Folder)
+    print(os.getcwd())
+    input("If this is not correct this WILL NOT work. Press enter to continue.")
+    os.chdir("nodejs/backup-bot")
+    os.system('cmd /c npm install')
+    input("Complete. Press enter to exit.")
+    exit()
+elif Bot_Choice == "3":
+    Repo_Folder = input("Enter the location of the cloned folder:")
+    os.chdir(Repo_Folder)
+    print(os.getcwd())
+    input("If this is not correct this WILL NOT work. Press enter to continue.")
+    os.chdir("nodejs")
+    os.system('cmd /c npm install')
+    os.chdir("backup-bot")
+    os.system('cmd /c npm install')
+    input("Complete. Press enter to exit.")
+    exit()
+else:
+    input("Whoops! That wasn't supposed to happen Press enter to exit.")
+    exit()
