7c7fa2b6ddd4b4e6d1fc15e50aa229590f13bb59
Lord-Giganticus <mrubins64@gmail.com>
2021-01-06 08:50:31 -0600

    Fixed a mistake in os.system and updated use of error()

--- run_bots.py
+++ run_bots.py
@@ -6,49 +6,52 @@ if bot_choice == "1":
     cmd_choice = input("Enter the number on which cmd program you want to use.\n[1] Node\n[2] PM2\n")
     if cmd_choice == "1":
         try:
-            os.system('cmd /k node_normal_bot.bat')
+            os.system('cmd /k start node_normal_bot.bat')
             complete()
+            os.system('cmd /c powershell taskkill /f /im node.exe')
             exit()
         except:
-            error()
+            error(8, 12)
     elif cmd_choice == "2":
         try:
-            os.system('cmd /c pm2_normal_bot.bat')
+            os.system('cmd /c start pm2_normal_bot.bat')
             complete()
             os.system('cmd /c pm2 delete among-us-bot')
             exit()
         except:
-            error()
+            error(16, 20)
     else:
-        error()
+        error(6, 0)
 elif bot_choice == "2":
     cmd_choice = input("Enter the number on which cmd program you want to use.\n[1] Node\n[2] PM2\n")
     if cmd_choice == "1":
         try:
-            os.system('cmd /k node_backup_bot.bat')
+            os.system('cmd /k start node_backup_bot.bat')
             complete()
+            os.system('cmd /c powershell taskkill /f /im node.exe')
             exit()
         except:
-            error()
+            error(28, 32)
     elif cmd_choice == "2":
         try:
-            os.system('cmd /c pm2_backup_bot.bat')
+            os.system('cmd /c start pm2_backup_bot.bat')
             complete()
             os.system('cmd /c pm2 delete backup-among-us-bot')
             exit()
         except:
-            error()
+            error(36, 40)
     else:
-        error()
+        error(26, 0)
 elif bot_choice == "3":
     cmd_choice = input("Enter the number on which cmd program you want to use.\n[1] Node\n[2] PM2\n")
     if cmd_choice == "1":
         try:
             run_both_bat()
             complete()
+            stop_both_bat()
             exit()
         except:
-            error()
+            error(48, 52)
     elif cmd_choice == "2":
         try:
             run_both_pm2()
@@ -56,8 +59,8 @@ elif bot_choice == "3":
             stop_both_pm2()
             exit()
         except:
-            error()
+            error(56, 60)
     else:
-        error()
+        error(46, 0)
 else:
-    error()
+    error(4, 0)
