69ec126c5f064dff3e085885e8be95d09ea6ad54
Lord-Giganticus <mrubins64@gmail.com>
2021-01-06 08:59:19 -0600

    Fixed small mistake in error and in stop_both_bat

--- framework.py
+++ framework.py
@@ -2,7 +2,7 @@ import os
 
 def error(x, y):
     if y == 0:
-        print('The Program had a error on line',str(x) +'.')
+        print('The Program had a error on line',str(x)+'.')
     elif y!= 0:
         print('The Program may have had a error on line',str(x),'to line',str(y)+'.')
     input('Press enter to exit.')
@@ -11,7 +11,7 @@ def run_both_bat():
     os.system('cmd /k start node_normal_bot.bat')
     os.system('cmd /k start node_backup_bot.bat')
 def stop_both_bat():
-    os.system('cmd /c powershell powershell taskkill /f /im node.exe')
+    os.system('cmd /c powershell taskkill /f /im node.exe')
 def run_both_pm2():
     os.system('cmd /c start pm2_normal_bot.bat && start pm2_backup_bot.bat')
 def stop_both_pm2():
