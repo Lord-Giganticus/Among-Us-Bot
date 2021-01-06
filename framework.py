e2b299b5c17c924708ff914d542b774b268200c6
Lord-Giganticus <mrubins64@gmail.com>
2021-01-06 08:49:15 -0600

    Updated framework to have upgraded error function and fixed some os.system mistakes.

--- framework.py
+++ framework.py
@@ -1,13 +1,19 @@
 import os
 
-def error():
-    input("Something went wrong! Press enter to exit")
+def error(x, y):
+    if y == 0:
+        print('The Program had a error on line',str(x) +'.')
+    elif y!= 0:
+        print('The Program may have had a error on line',str(x),'to line',str(y)+'.')
+    input('Press enter to exit.')
     exit() 
 def run_both_bat():
-    os.system('cmd /k node_normal_bot.bat')
-    os.system('cmd /k node_backup_bot.bat')
+    os.system('cmd /k start node_normal_bot.bat')
+    os.system('cmd /k start node_backup_bot.bat')
+def stop_both_bat():
+    os.system('cmd /c powershell powershell taskkill /f /im node.exe')
 def run_both_pm2():
-    os.system('cmd /c pm2_normal_bot.bat && pm2_backup_bot.bat')
+    os.system('cmd /c start pm2_normal_bot.bat && start pm2_backup_bot.bat')
 def stop_both_pm2():
     os.system('cmd /c pm2 delete among-us-bot && pm2 delete backup-among-us-bot')
 def complete():
